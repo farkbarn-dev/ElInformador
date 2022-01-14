5ddb76728f2d547d713dc5cac920b8a35baa1656
Frank Barrera | @farkbarn <frankbarreraag@gmail.com>
2022-01-14 11:52:35 -0500

    agregando header

--- header.php
+++ header.php
@@ -1,1 +1,16 @@
- 
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <title>El Informador Venezuela - Diario del Estado Lara > El Informador Venezuela</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, maximum-scale=1.0, minimal-ui, user-scalable=no">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="description" content="Diario El Informador" />
+    <meta name="robots" content="index,follow" />
+    <meta name="keywords" content="venezuela,lara,noticias,internacionales,deportes,sucesos,economia,actualidad,opinión,vida,especiales,carteles,obituarios"/>
+    <meta name="author" content="Frank Barrera | @farkbarn"/>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/responsive.css">
+    <link rel="shortcut icon" type="image/x-icon" href="img/logoEI.png">
+  </head>
