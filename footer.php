bc80589bcfafca501956afdc996d5df2ba1b2039
Frank Barrera | @farkbarn <frankbarreraag@gmail.com>
2022-01-14 11:52:20 -0500

    agregando footer

--- footer.php
+++ footer.php
@@ -1,1 +1,14 @@
- 
+        <section class="footer">
+          <section class="imgfooter">
+            <a class="aimgfooter" href="#"><img class="imgpietrio" src="img/Carteles-1.png" alt=""></a>
+            <a class="aimgfooter" href="#"><img class="imgpietrio" src="img/Clasificados-1.png" alt=""></a>
+            <a class="aimgfooter" href="#"><img class="imgpietrio" src="img/Obituarios-1.png" alt=""></a>
+          </section>
+          <section class="barrapie">
+
+          </section>
+        </section>
+      </section>
+    </main>
+  </body>
+</html>
