89e815650ee21672b4c7fe55b3a6cb7787146593
Frank Barrera | @farkbarn <frankbarreraag@gmail.com>
2022-02-22 10:11:57 -0500

    prueba menor con enlace directo de css desde la maquina virtual

--- header.php
+++ header.php
@@ -10,12 +10,14 @@
     <meta name="keywords" content="venezuela,lara,noticias,internacionales,deportes,sucesos,economia,actualidad,opinión,vida,especiales,carteles,obituarios"/>
     <meta name="author" content="Frank Barrera | @farkbarn"/>
     <link rel="stylesheet" href="<?php echo bloginfo('stylesheet_url'); ?>">
+    <link rel="stylesheet" href="http://192.168.1.39/wp-content/themes/elinformador/style.css">
     <link rel="stylesheet" href="<?php /// echo get_template_directory_uri()."/css/reset.css"; ?>">
     <link rel="stylesheet" href="<?php /// echo get_template_directory_uri()."/css/styles.css"; ?>">
     <link rel="stylesheet" href="<?php /// echo get_template_directory_uri()."/css/responsive.css"; ?>">
     <link rel="shortcut icon" type="image/x-icon" href="img/logoEI.png">
   </head>
   <body>
+    <?php ///echo localhost(); ?>
     <main class="main">
       <section class="header">
         <a class="alogo" href="<?php echo get_site_url(); ?>">
